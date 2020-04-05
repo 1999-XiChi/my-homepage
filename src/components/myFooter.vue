12fa39b465e27c995a3c968f7c5323cd87c88a5f
XiChi <474504964@qq.com>
2020-04-05 22:49:37 +0800

    Revert "security: update dependencies"
    This reverts commit 7021bae32d87269c250759d09d7041144f0fb862.
    
    	modify: myFooter

--- src/components/myFooter.vue
+++ src/components/myFooter.vue
@@ -9,7 +9,6 @@
         <div id="busuanzi_container_site_uv" class="uv">
            本站访客数<span id="busuanzi_value_site_uv"></span>人次
         </div>
-        <div class="record"><a href="http://www.beian.miit.gov.cn">浙ICP备19042074号-1</a></div>
         <img src="../assets/miku.png" alt="" />
       </div>
       <div class="links">
@@ -88,13 +87,11 @@ export default {
       .pv, .uv
         font-size 12px
         color rgba(255,255,255,0.5)
-      .record a
-        color #ef632b
       img
         display block
         margin 0 auto
-        width auto
-        height 1rem
+        width 1.4rem
+        height auto
 @media screen and (max-width: 767px)
   .footer-wrap
     .footer
