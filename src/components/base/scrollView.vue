ab67ca0e7eef4f7703801ca18e937960044676f4
XiChi <474504964@qq.com>
2020-05-07 23:33:59 +0800

    add: more page

---
+++ src/components/base/scrollView.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="scroll-wrap" :style="{ width: width, height: height }">
+    <div class="scroll-container" ref="container">
+      <div class="scroll-content" ref="content">
+        <slot></slot>
+      </div>
+    </div>
+    <div class="scroll-bar" ref="bar" :style="scrollbarStyle"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    width: {
+      type: String,
+      default: "60%",
+    },
+    height: {
+      type: String,
+      default: "80%",
+    },
+    scrollbarStyle: Object,
+  },
+  methods: {
+    moveScrollBar() {
+      const container = this.$refs.container;
+      const content = this.$refs.content;
+      const bar = this.$refs.bar;
+      const rate = container.clientHeight / content.scrollHeight;
+      const barHeight = rate * container.clientHeight;
+      if (rate < 1) {
+        //需要出现滚动条,并计算滚动条的高度
+        bar.style.height = barHeight + "px";
+      } else {
+        //不需要出现滚动条
+        bar.style.display = "none";
+      }
+      container.addEventListener('scroll',(e)=>{
+        bar.style.top = e.target.scrollTop * rate + "px";
+      })
+    },
+  },
+  mounted(){
+    this.moveScrollBar();
+  }
+};
+</script>
+
+<style lang="stylus" scoped>
+.scroll-wrap
+  overflow hidden
+  margin 0 auto
+  position relative
+  .scroll-bar
+    position absolute
+    top 0
+    right 0
+    border-radius 5px
+    width 5px
+    height 30%
+    background-color rgba(255,255,255,.6)
+  .scroll-container
+    width calc(100% + 18px) /* 挡住原生scrollbar */
+    height 100%
+    overflow auto
+    .scroll-content
+      width 100%
+</style>
--- src/pages/cards/more.vue
+++ src/pages/cards/more.vue
@@ -1,15 +1,81 @@
 <template>
-  <div>
-    留言板正在开发ing
+  <div class="more">
+    <scroll-view
+      class="message-board"
+      :scrollbarStyle="{ backgroundColor: 'rgba(15, 76, 129, 0.8)' }"
+    >
+      <template v-slot>
+        <div class="message-content">
+          <div class="header">欢迎给我留言~</div>
+          <div class="new_message">
+            <div :style="{
+              display: 'flex',
+              flexDirection: 'column'
+            }">
+              <input type="text" placeholder="Your Name" />
+              <textarea rows="5" cols="50" placeholder="Your Message">
+              </textarea>
+              <div>
+                <span>还能输入140个字</span>
+                <input type="submit" value="发表留言">
+              </div>
+            </div>
+          </div>
+          <div class="message-list-title">
+            <span>留言墙</span>
+          </div>
+          <div class="message-list">
+            <div class="message-card"></div>
+            <div class="message-card"></div>
+            <div class="message-card"></div>
+            <div class="message-card"></div>
+          </div>
+        </div>
+      </template>
+    </scroll-view>
   </div>
 </template>
 
 <script>
+import scrollView from "_c/base/scrollView";
 export default {
-  
-}
+  components: {
+    scrollView,
+  },
+  data() {
+    return {
+      messageList: [],
+    };
+  },
+};
 </script>
 
 <style lang="stylus" scoped>
-
+.more
+  height 100vh
+  display flex
+  align-items center
+  .message-board
+    color #000
+    font-size 15px
+    border-radius 20px
+    background-color rgba(255,255,255,.9)
+    .message-content
+      padding 10px 10%
+      .message-list-title
+        background-color #d4dee1
+        height 40px
+        > span 
+          float left
+          margin 10px 10px 0 10px
+          padding 0 20px
+          line-height 30px
+          background-color #ffffff
+          border-radius 10px 10px 0 0
+      .message-list
+        .message-card
+          height 150px
+          border-bottom 1px dashed #999
+    .box
+      height 500px
 </style>
